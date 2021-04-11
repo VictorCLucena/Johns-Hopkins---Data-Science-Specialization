c1a5d794bc9bea406e090791fe94bf5665cc9939
Victor Lucena <lucena1990@gmail.com>
2021-04-10 23:52:35 -0400

    files

---
+++ Curso 03 - Getting and Cleaning Data/Week04/Final Project/run_analysis.R
@@ -0,0 +1,12 @@
+featnames <- read.table("./UCI HAR Dataset/features.txt")
+featnames <- as.vector(featnames$V2)
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+train_df <- cbind()
