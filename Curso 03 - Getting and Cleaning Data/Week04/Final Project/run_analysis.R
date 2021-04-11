6c715ce8f711ef7b61b33e3206bc972a65093dec
Victor Lucena <lucena1990@gmail.com>
2021-04-11 00:11:15 -0400

    colnames added

--- Curso 03 - Getting and Cleaning Data/Week04/Final Project/run_analysis.R
+++ Curso 03 - Getting and Cleaning Data/Week04/Final Project/run_analysis.R
@@ -1,12 +1,17 @@
+library(dplyr)
+
 featnames <- read.table("./UCI HAR Dataset/features.txt")
 featnames <- as.vector(featnames$V2)
 
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = featnames)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "Y")
 
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = featnames)
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "Y")
 
-train_df <- cbind()
+train_df <- cbind(subject_train, y_train, X_train)
+test_df <- cbind(subject_test, y_test, X_test)
+df <- rbind(train_df, test_df)
+
